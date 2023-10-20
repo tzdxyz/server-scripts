08181b6b48c9fc1d18cd7fa57cdc39356abe35e9
tom <23652063+tzdxyz@users.noreply.github.com>
2023-10-20 21:13:02 +0800

    some changes

--- network/setup-ssh.sh
+++ network/setup-ssh.sh
@@ -2,8 +2,28 @@
 # change ssh configs
 # ref: https://stackoverflow.com/questions/36389561/bash-script-to-disable-root-login
 
+config_file="/etc/ssh/sshd_config"
 
 # shutdown root login
-sed -i '/^PermitRootLogin[ \t]\+\w\+$/{ s//PermitRootLogin no/g; }' /etc/ssh/sshd_config
+sed -i '/^PermitRootLogin[ \t]\+\w\+$/{ s//PermitRootLogin no/g; }' $config_file
 # limit max auth tries
-sed -i '/^#MaxAuthTries[ \t]\+\w\+$/{ s//MaxAuthTries 6/g; }' /etc/ssh/sshd_config
+sed -i '/^#MaxAuthTries[ \t]\+\w\+$/{ s//MaxAuthTries 6/g; }' $config_file
+
+
+# Check if the ClientAliveInterval setting already exists
+if grep -q '^ClientAliveInterval' "$config_file"; then
+  # If the setting exists and is commented or has a different value, update it
+    sed -Ei 's/^#?(ClientAliveInterval).*/\1 60/' "$config_file"
+else
+  # If the setting does not exist, add it to the end of the file
+    echo "ClientAliveInterval 60" >> $config_file
+fi
+
+# Check if the ClientAliveCountMax setting already exists
+if grep -q '^ClientAliveCountMax' "$config_file"; then
+  # If the setting exists and is commented or has a different value, update it
+    sed -Ei 's/^#?(ClientAliveCountMax).*/\1 3/' "$config_file"
+else
+  # If the setting does not exist, add it to the end of the file
+    echo "ClientAliveCountMax 3" >> $config_file
+fi
