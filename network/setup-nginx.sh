6920004b617a3448792f5f05a6f9dc413255c8ac
tom <23652063+tzdxyz@users.noreply.github.com>
2023-10-21 14:22:52 +0800

    some changes

--- network/setup-nginx.sh
+++ network/setup-nginx.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # setup nginx after installation
 
-sudo systemctl start nginx
 sudo systemctl enable nginx
-
+sudo systemctl start nginx
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https	
+sudo firewall-cmd --reload
