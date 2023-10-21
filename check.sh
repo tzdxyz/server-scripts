649e1998991522eb5bf0a97b1503d9c8ba359e5e
tom <23652063+tzdxyz@users.noreply.github.com>
2023-10-21 14:39:00 +0800

    some changes

--- check.sh
+++ check.sh
@@ -8,6 +8,7 @@ last | head -n 5
 # check network
 echo -e "\ntcp connection: "
 ss -tp
+ss -tpln
 
 # check firewalld
 echo -e "\nfirewalld running state: "
@@ -16,6 +17,10 @@ sudo firewall-cmd --state
 # firewalld configuration
 echo -e "\nfirewalld config state: "
 sudo firewall-cmd --get-active-zone
+
+# check nginx
+echo -e "\nnginx running state: "
+sudo systemctl status nginx --no-pager | grep Active:
 
 # check docker 
 echo -e "\ndocker engine running state: "
--- network/setup-nginx.sh
+++ network/setup-nginx.sh
@@ -2,7 +2,7 @@
 # setup nginx after installation
 
 sudo systemctl enable nginx
-sudo systemctl start nginx
 sudo firewall-cmd --permanent --add-service=http
 sudo firewall-cmd --permanent --add-service=https	
 sudo firewall-cmd --reload
+sudo systemctl start nginx
